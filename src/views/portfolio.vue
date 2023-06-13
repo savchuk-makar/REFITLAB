f7ee2a0927b2e1bce1d06d1780e3e70ea1b27de4
Makar Savchuk <makar.savchuk.2020@gmail.com>
2023-06-13 16:34:12 +0300

    add

--- src/views/portfolio.vue
+++ src/views/portfolio.vue
@@ -1,76 +1,98 @@
 <template>
-                <div class="dark:bg-gray-900 relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8 -mb-32">
-              <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                <span class="block text-white text-5xl">Данный раздел находится в разработке</span>
-              </h1>
-              <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
-              </div>
-            </div>
-<!-- component -->
-<section class="bg-white dark:bg-gray-900">
-    <div class="container px-6 py-10 mx-auto animate-pulse">
-        <h1 class="w-48 h-2 mx-auto bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-
-        <p class="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-        <p class="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg sm:w-80 dark:bg-gray-700"></p>
-
-        <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
-            <div class="w-full ">
-                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-                
-                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-            </div>
-
-            <div class="w-full ">
-                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-                
-                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-            </div>
-
-            <div class="w-full ">
-                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-                
-                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-            </div>
-
-            <div class="w-full ">
-                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-                
-                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-            </div>
-
-            <div class="w-full ">
-                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-                
-                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-            </div>
-
-            <div class="w-full ">
-                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-                
-                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-            </div>
-
-            <div class="w-full ">
-                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-                
-                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-            </div>
-
-            <div class="w-full ">
-                <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-                
-                <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-            </div>
-        </div>
+    <!-- 1 -->
+<div class="relative dark:bg-gray-900 pt-16  px-4 sm:px-6 lg:pt-24  lg:px-8">
+  <div class="absolute inset-0">
+    <div class="dark:bg-gray-900 h-1/3 sm:h-2/3"></div>
+  </div>
+  <div class="relative max-w-7xl mx-auto">
+    <div class="text-center">
+      <h2 class="text-3xl tracking-tight font-extrabold dark:text-white sm:text-4xl">Наши работы</h2>   
+      <p class="mt-3 mx-auto text-xl dark:text-white sm:mt-4">Все работы производятся специалистами с использованием современного оборудования и качественных материалов. <br> Мы гарантируем качество выполненных работ и предоставляем гарантию на все установленные запчасти.</p>
     </div>
-</section>
+    <!-- Product List -->
+<!-- component -->
+<div class="relative min-h-screen flex flex-col items-center justify-center "> 
+
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/1.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/2.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/3.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/4.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/5.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/6.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/7.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/8.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/9.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/10.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/11.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/12.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/13.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/14.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/15.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/16.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/17.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/18.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/19.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/20.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/21.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/22.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/23.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="@/assets/images/portfolio/24.jpg" alt="">
+    </div>
+</div>
+
+</div>
+  </div>
+</div>
+
 </template>
+<script setup>
+import ButtonBtn1 from "@/components/button-btn-1.vue";
+</script>

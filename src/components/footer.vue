e84a6594c71e2ed74867cf6e0992bbc5df7b86f8
Makar Savchuk <makar.savchuk.2020@gmail.com>
2023-06-28 16:43:57 +0300

    ad

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-white dark:bg-gray-900">
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
       <div class="mt-8 md:mt-0 md:order-1">
-        <p class="text-center text-base text-gray-400">&copy; 2017- 2023 Ремонт и тюнинг фар в Сочи | REFIT LAB</p>
+        <p class="text-center text-base text-gray-400">&copy; 2017-2023 Ремонт и тюнинг фар в Сочи | REFIT LAB <br>Сайт сделан в <a href="https://adwos.ru">adwos.ru</a></p>
       </div>
     </div>
   </footer>
--- src/components/header.vue
+++ src/components/header.vue
@@ -69,6 +69,8 @@
       </div>
     </nav>
   </header>
+
+  
 </template>
 
 <script setup>
